53af3954acb55bedf3ed9fdb32b0521e276496ed
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2022-12-06 12:19:27 -0700

    Add medium challenge code

---
+++ code_challenges/medium_challenge.rb
@@ -0,0 +1,10 @@
+good_driving_record = true
+age = 24
+
+# if good_driving_record && age > 25
+#     puts "Car rental discout"
+#     elsif good_driving_record || age > 25
+#         puts "Pay full price"
+#     else 
+#         puts "Sign for rental"
+#     end
