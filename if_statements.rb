3c21ee4c69d8a953e10bb9b8cd1c192ef5800f85
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2022-12-06 11:49:12 -0700

    Add pizza variables

--- if_statements.rb
+++ if_statements.rb
@@ -70,3 +70,33 @@ end
 
 cups_of_flour = 1
 has_sauce = true
+
+cups_of_flour = 5
+has_sauce = false
+
+cups_of_flour = 2
+has_sauce = true
+
+cups_of_flour = 3
+has_sauce = true
+
+if cups_of_flour == 1 && has_sauce == true
+  puts "I cannot make pizza"
+end
+
+if cups_of_flour == 5 && has_sauce == false
+  puts "I cannot make pizza"
+end
+
+if cups_of_flour == 2 && has_sauce == true
+  puts "I can make pizza"
+end
+
+if cups_of_flour == 3 && has_sauce == true
+  puts "I can make pizza"
+end
+
+
+
+
+
